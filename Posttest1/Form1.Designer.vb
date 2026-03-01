8f2c4fb40c7ff8412c2095af067ae7ee30a6c7b4
ACER <ACER@LAPTOP-GGT481MF>
2026-03-01 19:19:29 +0800

    Add project files.

---
+++ Posttest1/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txtIPSemester = New TextBox()
+        txtIPKumulatif = New TextBox()
+        btnTambah = New Button()
+        btnReset = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        lblPredikat = New Label()
+        SuspendLayout()
+        ' 
+        ' txtIPSemester
+        ' 
+        txtIPSemester.Location = New Point(324, 143)
+        txtIPSemester.Name = "txtIPSemester"
+        txtIPSemester.Size = New Size(176, 27)
+        txtIPSemester.TabIndex = 0
+        ' 
+        ' txtIPKumulatif
+        ' 
+        txtIPKumulatif.Location = New Point(324, 211)
+        txtIPKumulatif.Name = "txtIPKumulatif"
+        txtIPKumulatif.ReadOnly = True
+        txtIPKumulatif.Size = New Size(176, 27)
+        txtIPKumulatif.TabIndex = 1
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(324, 176)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 2
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(424, 176)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(76, 29)
+        btnReset.TabIndex = 3
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(211, 146)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 4
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(211, 218)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(324, 259)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(53, 20)
+        lblPredikat.TabIndex = 6
+        lblPredikat.Text = "Label3"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblPredikat)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIPKumulatif)
+        Controls.Add(txtIPSemester)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents txtIPSemester As TextBox
+    Friend WithEvents txtIPKumulatif As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblPredikat As Label
+
+End Class
---
+++ Posttest1/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+    Private txtPredikat As Object
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim ipSemester As Double
+
+        If Double.TryParse(txtIPSemester.Text, ipSemester) Then
+
+            totalIP = totalIP + ipSemester
+            jumlahSemester = jumlahSemester + 1
+
+            txtIPKumulatif.Text = totalIP.ToString("0.00")
+
+            Dim rataIP As Double = totalIP / jumlahSemester
+
+            If rataIP >= 2.0 And rataIP <= 2.75 Then
+                lblPredikat.Text = "Cukup"
+            ElseIf rataIP >= 2.76 And rataIP <= 3.0 Then
+                lblPredikat.Text = "Memuaskan"
+            ElseIf rataIP > 3.0 Then
+                lblPredikat.Text = "Sangat Memuaskan"
+            Else
+                lblPredikat.Text = "-"
+            End If
+
+            txtIPSemester.Text = ""
+
+        Else
+            MessageBox.Show("IP Semester harus berupa angka!", "Peringatan")
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+
+        txtIPSemester.Text = ""
+        txtIPKumulatif.Text = ""
+        lblPredikat.Text = ""
+    End Sub
+
+End Class
+
